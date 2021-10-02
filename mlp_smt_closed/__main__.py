e9c43ac3e9f0d1dbe6ebf0e4741d52c53752c0b0
Nicolai Vincent Radke <nicolai.radke@rwth-aachen.de>
2021-10-02 16:38:50 +0200

    extend argument parser

--- mlp_smt_closed/__main__.py
+++ mlp_smt_closed/__main__.py
@@ -1,42 +1,78 @@
 """This module contains the main function which starts our software depending on the arguments specified"""
 
-import time
-import os
-import sys
-import inspect
-
-from numpy.lib import polynomial
-
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-from logging import log
 from mlp_smt_closed.arguments import *
 from mlp_smt_closed.smt.logic import *
 from mlp_smt_closed.smt.templates import *
 from mlp_smt_closed.mlp.functions import *
 
 if __name__ == '__main__':
-    
-    # TODO Arguments are ignored for now
+
+    # set encoding type
+    encoding = 'Real'
+    # encoding = 'FP'
+
+    # parse arguements
     args = parse_args()
+
+    '''
+    # load keras model to get output dimension (necessary for template-choice, currently only n x n matrices
+    # supported
+    model = keras.models.load_model(args.model)
+    output_dimension = model.layers[-1].output_shape[1]
+
+    # distinguish between linear and polynomial template
+    if args.template == 'linear':
+        template = GenericLinearTemplate(encoding=encoding, n=output_dimension, m=output_dimension)
+    # else: 'polynomial' was chosen
+    else:
+        template = PolynomialTemplate(degree=args.degree,variables=1)
+
+    # init adaptor
+    adaptor = Adaptor(args.model, template, args.intervals, splits=args.splits, encoding=encoding)
+
+    # distinguish between smt and least squares
+    if args.method == 'smt':
+        # distinguish between adjust and optimize
+        if args.optimize:
+            adaptor.optimize_template()
+        else:
+            adaptor.adjust_template(epsilon = args.epsilon)
+    # else: least squares (ls) was chosen
+    else:
+
+        if args.template == 'linear':
+            if output_dimension == 1:
+                func_class = LinearA()
+            elif output_dimension == 2:
+                func_class = LinearA2D()
+            elif output_dimension == 15:
+                func_class = Platoon()
+            else:
+                print("Error: No support for this dimension yet. Try 'smt' instead of 'ls'.")
+            adaptor.regression_verification_nd(func_class, [101, 101], 0.5, 4)
+        else:
+            if args.degree == 1:
+                func_class = LinearA()
+            elif args.degree == 2:
+                func_class = QuadraticA()
+            elif args.degree == 3:
+                func_class = PolyDeg3()
+            else:
+                print("Error: No support for this degree yet. Try 'smt' instead of 'ls'.")
+            adaptor.polyfit_verification_1d(func_class, 200, 0.5, 4)
+    '''
 
     # extract path to trained keras-model from arguments
     model_path = args.model
+
 
     start_time_overall = time.time()
 
     # set func_class, only necessary for if polyfit or regression used
     func_class = LinearA2D()
 
-    # set encoding type
-    encoding = 'Real'
-    #encoding = 'FP'
-
-
     # myLinTemplate = LinearTemplate(encoding=encoding)
-    myLinTemplate = Linear2DTemplate()
+    myLinTemplate = GenericLinearTemplate(encoding=encoding, n=2, m=2)
     # polyTemp = PolynomialTemplate(degree=func_class.degree(),variables=1)
 
     # just for debugging purposes
@@ -65,7 +101,7 @@ if __name__ == '__main__':
     print('Overall computation time: ', end_time_overall - start_time_overall, 'seconds')
 
     # Test template optimization
-    #myAdaptor.optimize_template()
+    # myAdaptor.optimize_template()
 
     '''# LinearA2D was trained on ((-10,-10),(10,10))
     myLinTemplate = Linear2DTemplate()
--- mlp_smt_closed/arguments.py
+++ mlp_smt_closed/arguments.py
@@ -3,6 +3,9 @@ import argparse
 
 
 def _is_valid_file(arg):
+    """
+    Type check for optional argument '--model'. A path to an .h5 Keras model file should be passed.
+    """
 
     if not os.path.exists(arg):
         raise argparse.ArgumentTypeError('{} not found!'.format(arg))
@@ -17,14 +20,32 @@ def parse_args():
     Specifies valid arguments
     """
 
+    # default path to model
+    path_to_model = os.path.join('mlp_smt_closed', 'mlp', 'models', 'LinearA2D_model.h5')
+
+    # init parser
     parser = argparse.ArgumentParser(description="DESCRIPTION", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     # Optional Arguments
-
-    path_to_model = os.path.join('mlp_smt_closed','mlp', 'models', 'LinearA2D_model.h5')
-    parser.add_argument('-model', help='Path to Keras .h5 model file', default=path_to_model, type=_is_valid_file)
-
-    # parser.add_argument('-template', help='Specification of which template is to be configured') #possibly enum type?
+    parser.add_argument('-m', '--model', default=path_to_model, type=_is_valid_file,
+                        help='path to Keras .h5 model file')
+    parser.add_argument('--method', default='smt', choices=['smt', 'ls'],
+                        help='to find parameters choose either the SMT (smt) method or the least-squares (ls) method')
+    parser.add_argument('-o', '--optimize', action='store_true',
+                        help='chose whether the parameters should not only be found but also optimized. Only relevant '
+                             'when also choosing "--method smt"')
+    parser.add_argument('-s', '--splits', default=0, type=int,
+                        help='#splits + 1 threads are used')
+    parser.add_argument('-t', '--template', default='linear', choices=['linear', 'polynomial'],
+                        help='choose to fit the MLP to a linear or polynomial function')
+    parser.add_argument('-d', '--degree', default=1, type=int, help='degree of the polynomial')
+    parser.add_argument('-i', '--intervals', default=((-8,), (8,)), help='interval in which the parameters should fit')
+    parser.add_argument('--sizes', default=[1001], type=int, help='number of samples taken in case "--method ls"')
+    parser.add_argument('-e', '--epsilon', default=0.5,
+                        help='Attention: the interpretation of epsilon is context dependent! Read the README or the '
+                             'Documentation for details.')
+    parser.add_argument('--steps', default=4, type=int,
+                        help='number of interval refinement steps taken in case "--method ls"')
 
     args = parser.parse_args()
 
